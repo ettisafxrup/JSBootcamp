1ba23cf8bd5eb87ca91a18d9cbf1224520981310
ettisafxrup <ettisafrup@gmail.com>
2025-06-23 00:05:42 +0600

    JS_01

---
+++ Codes/JavaScript_01_data_types.js
@@ -0,0 +1,71 @@
+// 2. Data Types
+
+// 2:
+// 1. Primitive
+// 2. Non-Primitive
+
+// Primitive Datatypes
+// Plain, no methods or objects included.
+
+// # String
+// let userName = "Dip Paul"
+// console.log(typeof userName)
+
+// Quick Quiz
+// let salary = "500000" // String
+
+// # Number
+// let salary = 500000
+// console.log(typeof salary)
+
+// # Boolean
+// let isActive = true
+// console.log(typeof isActive)
+
+// # Undefined
+// let maritalStatus
+// console.log(maritalStatus)
+
+// # Null (Intentional Absence of a variable)
+// let jobStatus = null
+// console.log(jobStatus)
+
+// Q. Why null datatype's datatype says Object?
+// => It's just a bug.
+
+// console.log(typeof null)
+
+// !! Non-Primitive Datatypes
+// # Objects
+// {} => Curly Bracks
+
+// let jobStatus = "Employed"
+
+// let user = {
+//   name: "John Doe",
+//   age: 22,
+//   isMarried: false,
+//   jobStatus,
+// }
+
+// console.log(user.name)
+// console.log(user.age)
+// console.log(user.isMarried)
+
+// console.log(user.jobStatus)
+
+// # Array
+// Sum-up of All kind of primitive datas
+
+// [] => Square Brackets
+// let array = ["Rup", 20, true]
+
+// console.log(array[0])
+// console.log(array[1])
+// console.log(array[2])
+
+// Function
+// >> Next Class
+
+// Date
+// RegExp
---
+++ Codes/JavaScript_01_variables.js
@@ -0,0 +1,41 @@
+// JS01 -> Variables and Data Types
+
+// Portion 01: Datatype
+// Variable
+// 1999 -> 2015
+
+// 2015 -> var
+// var x = "Rup"
+// var y = "Surjo"
+
+// 2015 -> ES6+
+// let, const (word 'var')
+
+// ❌ discouraged to use var to define a variable.
+
+// why do var exists tho?
+// 1. Old Websites
+// 2. Old Browsers
+
+// let x = "Rup"
+// const y = "Hridoy"
+
+// let x = "Hridoy"
+// let y = "Dip"
+// const pi = 3.1416
+
+// console.log("x: ", x)
+// console.log("y: ", y)
+
+// x = "Rup"
+// console.log("x: ", x)
+
+// y = "Ushashi"
+// console.log("y: ", y)
+
+// let userName = "Mr. Demo"
+// console.log("Hello, ", userName)
+// userName = "Ettisaf Rup"
+// console.log("Hello, ", userName)
+// Summary:
+// 1. While Declearing a Variable, we will be always using let, and const keywords. Trying our best to avoid var.

8b43991541f05282358955bf5a096b604d1687a0
ettisafxrup <ettisafrup@gmail.com>
2025-06-28 20:39:52 +0600

    JS05

---
+++ Codes/JS05/dom.js
@@ -0,0 +1,47 @@
+// DOM Class
+
+// Accessing Elements by ID
+// const helloTag = document.getElementById("id")
+// console.log(helloTag)
+
+// helloTag.innerText = "Noob"
+
+// Accessing Elements by Class Name
+
+// const hiTag = document.getElementsByClassName("class")[1]
+
+// console.log(hiTag)
+// hiTag.innerText = "jello"
+
+// Accessing Elements by Tag Name
+
+// const tags = document.getElementsByTagName("p")
+
+// console.log(tags)
+
+// const userName = "Ettisaf Rup"
+
+// const helloTag = document.getElementById("id")
+
+// // Hello User, Welcome to our Website.
+// helloTag.innerText = `Hello ${userName}, welcome to our Website`
+
+// const testDiv = document.getElementsByClassName("test")[0]
+
+// const userName = "Ayesha"
+// testDiv.innerHTML = `<h1>${userName}</h1>`
+
+// const body = document.body
+// body.style.backgroundColor = "red"
+
+// const pTag = document.getElementById("id")
+
+// pTag.style.color = "red"
+
+// Evenets
+// const pTag = document.getElementById("id")
+
+// pTag.addEventListener("mouseout", function () {
+//   pTag.style.color = "red"
+//   pTag.innerText = "Red Alert"
+// })
